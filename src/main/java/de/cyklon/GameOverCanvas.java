6b64521dd40e869f6e9a3f6db6cb44f59487bea1
cyklon73 <cyklon698@gmail.com>
2024-04-10 10:54:54 +0200

    added Shortcuts for button

--- src/main/java/de/cyklon/GameOverCanvas.java
+++ src/main/java/de/cyklon/GameOverCanvas.java
@@ -2,16 +2,21 @@ package de.cyklon;
 
 import de.cyklon.jui.UICanvas;
 import de.cyklon.jui.component.UIButton;
+import de.cyklon.jui.component.UICanvasButton;
 import de.cyklon.jui.component.UILabel;
+import de.cyklon.jui.input.Shortcut;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class GameOverCanvas extends UICanvas {
 
     public GameOverCanvas() {
         setBgColor(new Color(41, 222, 141));
         addComponents(new UILabel(250, 150, "Game Over"));
-        UIButton button = new UIButton(250, 250, 100, 30, (app, btn) -> app.setCanvas(new GameCanvas(app)));
+        UIButton button = new UICanvasButton(250, 250, 100, 30, GameCanvas::new);
+        button.addShortcut(Shortcut.ofKeyboard(KeyEvent.VK_ENTER));
+        button.addShortcut(Shortcut.ofKeyboard(KeyEvent.VK_SPACE));
         UILabel label = new UILabel(20, 20, "Play Again");
         label.setColor(new Color(200, 200, 200));
         button.addComponents(label);
--- src/main/java/de/cyklon/MenuCanvas.java
+++ src/main/java/de/cyklon/MenuCanvas.java
@@ -2,15 +2,20 @@ package de.cyklon;
 
 import de.cyklon.jui.UICanvas;
 import de.cyklon.jui.component.UIButton;
+import de.cyklon.jui.component.UICanvasButton;
 import de.cyklon.jui.component.UILabel;
+import de.cyklon.jui.input.Shortcut;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class MenuCanvas extends UICanvas {
 
     public MenuCanvas() {
         setBgColor(new Color(41, 222, 141));
-        UIButton button = new UIButton(250, 250, 100, 30, (app, btn) -> app.setCanvas(new GameCanvas(app)));
+        UIButton button = new UICanvasButton(250, 250, 100, 30, GameCanvas::new);
+        button.addShortcut(Shortcut.ofKeyboard(KeyEvent.VK_ENTER));
+        button.addShortcut(Shortcut.ofKeyboard(KeyEvent.VK_SPACE));
         UILabel label = new UILabel(40, 20, "Play");
         label.setColor(new Color(200, 200, 200));
         button.addComponents(label);
