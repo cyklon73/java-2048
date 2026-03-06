de939bbe906717761cd85d56107454526f51fcd9
cyklon73 <cyklon698@gmail.com>
2026-03-06 08:41:38 +0100

    update

--- src/main/java/de/cyklon/Block.java
+++ src/main/java/de/cyklon/Block.java
@@ -11,21 +11,23 @@ public class Block extends UIComponent {
 
     private static final Font FONT = new Font("Arial", Font.BOLD, 54);
 
-    private static final Map<Integer, Color> colors = Map.of(
-            2, new Color(238, 228, 218),
-            4, new Color(237, 224, 200),
-            8, new Color(242, 177, 121),
-            16, new Color(245, 149, 99),
-            32, new Color(246, 124, 95),
-            64, new Color(246, 94, 59),
-            128, new Color(237, 207, 114),
-            256, new Color(237, 204, 97),
-            512, new Color(237, 200, 80),
-            1024, new Color(237, 197, 63)
+    private static final Map<Integer, Color> colors = Map.ofEntries(
+            Map.entry(0, new Color(204, 190, 179)),
+            Map.entry(2, new Color(238, 228, 218)),
+            Map.entry(4, new Color(237, 224, 200)),
+            Map.entry(8, new Color(242, 177, 121)),
+            Map.entry(16, new Color(245, 149, 99)),
+            Map.entry(32, new Color(246, 124, 95)),
+            Map.entry(64, new Color(246, 94, 59)),
+            Map.entry(128, new Color(237, 207, 114)),
+            Map.entry(256, new Color(237, 204, 97)),
+            Map.entry(512, new Color(237, 200, 80)),
+            Map.entry(1024, new Color(237, 197, 63))
     );
 
     private int value;
     private Color color;
+    private boolean blocked = false;
 
     public Block(int value, int x, int y, int width, int height) {
         super(x, y, width, height);
@@ -38,20 +40,29 @@ public class Block extends UIComponent {
 
     public void setValue(int value) {
         this.value = value;
-        if (value!=0) this.color = colors.getOrDefault(value, Color.WHITE);
+        this.color = colors.getOrDefault(value, Color.WHITE);
     }
 
     @Override
     protected void render(App app, Graphics g) {
-        if (value==0) return;
         g.setColor(color);
         g.fillRect(x, y, width, height);
         g.setColor(new Color(60, 60, 60));
+
+        if (value==0) return;
 
         FontRenderer fr = new FontRenderer(g);
         double dig = Math.log10(value);
         fr.setFont(FONT.deriveFont((float) (54 - dig*6)));
         fr.setAlignment(FontRenderer.Alignment.CENTERED);
         fr.drawString(String.valueOf(value), x + width/2, (int) (y + 55 - dig*2));
+    }
+
+    public boolean isBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        this.blocked = blocked;
     }
 }
--- src/main/java/de/cyklon/GameCanvas.java
+++ src/main/java/de/cyklon/GameCanvas.java
@@ -2,30 +2,42 @@ package de.cyklon;
 
 import de.cyklon.jui.App;
 import de.cyklon.jui.UICanvas;
+import de.cyklon.jui.component.UILabel;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.util.Arrays;
 import java.util.Random;
 
 public class GameCanvas extends UICanvas {
 
+    private static final int spawnValue = 2;
+    private static final long animationDelay = 25;
+
     private final Random random = new Random();
     private final Block[][] blocks = new Block[4][4];
+    private int score = 0;
+
+    private boolean moving = false;
 
     public GameCanvas(App app) {
-        setBgColor(new Color(41, 222, 141));
+        setBgColor(new Color(186, 172, 159));
         int i1 = 0;
-        for (int x = 50; x < (80+50)*4; x+=80+50) {
+        for (int x = 50; x < (80+50)*blocks.length; x+=80+50) {
             int i2 = 0;
-            for (int y = 50; y < (80+50)*4; y+=80+50) {
-                addComponents(blocks[i1][i2] = new Block(0, x, y, 80, 80));
-                i2++;
-            }
+            for (int y = 50; y < (80+50)*blocks[0].length; y+=80+50)
+                addComponents(blocks[i1][i2++] = new Block(0, x, y, 80, 80));
             i1++;
         }
+
+        UILabel scoreLabel = new UILabel(50, 25);
+        addComponents(scoreLabel);
+
         spawn(app);
         spawn(app);
         app.runTask(a -> {
+            scoreLabel.setText("Score: " + score);
+            if (moving) return;
             if (a.getKeyboard().isClicked(KeyEvent.VK_RIGHT)) moveRight(app, true);
             if (a.getKeyboard().isClicked(KeyEvent.VK_LEFT)) moveLeft(app, true);
             if (a.getKeyboard().isClicked(KeyEvent.VK_UP)) moveUp(app, true);
@@ -56,16 +68,14 @@ public class GameCanvas extends UICanvas {
     }
 
     private boolean isSpace() {
-        boolean free = false;
         for (Block[] block : blocks) {
             for (Block block1 : block) {
                 if (block1.getValue() == 0) {
-                    free = true;
-                    break;
+                    return true;
                 }
             }
         }
-        return free;
+        return false;
     }
 
     private void spawn(App app) {
@@ -74,99 +84,178 @@ public class GameCanvas extends UICanvas {
         int y;
 
         do {
-            x = random.nextInt(4);
-            y = random.nextInt(4);
+            x = random.nextInt(blocks.length);
+            y = random.nextInt(blocks[0].length);
         } while (blocks[x][y].getValue()!=0);
 
-        blocks[x][y].setValue(2);
+        blocks[x][y].setValue(spawnValue);
         if (isGameOver()) app.setCanvas(new GameOverCanvas());
     }
 
-    private void moveRight(App app, boolean initial) {
-        boolean moved = false;
-        for (int x = blocks.length - 1; x >= 0; x--) {
-            if (x!=blocks.length-1) {
-                for (int y = 0; y < blocks[x].length; y++) {
-                    if (blocks[x][y].getValue()!=0) {
-                        if (blocks[x+1][y].getValue()==0) {
-                            blocks[x+1][y].setValue(blocks[x][y].getValue());
-                            blocks[x][y].setValue(0);
-                            moved = true;
-                        } else if (blocks[x+1][y].getValue()==blocks[x][y].getValue()) {
-                            blocks[x+1][y].setValue(blocks[x][y].getValue()*2);
-                            blocks[x][y].setValue(0);
-                        }
-                    }
-                }
-            }
-        }
-        if (moved) moveRight(app, false);
-        else if (!initial) spawn(app);
+    private void endMove() {
+        for (Block[] block : blocks)
+            for (Block value : block)
+                value.setBlocked(false);
+        moving = false;
     }
 
-    private void moveLeft(App app, boolean initial) {
-        boolean moved = false;
-        for (int x = 0; x < blocks.length; x++) {
-            if (x!=0) {
-                for (int y = 0; y < blocks[x].length; y++) {
-                    if(blocks[x][y].getValue()!=0) {
-                        if (blocks[x-1][y].getValue()==0) {
-                            blocks[x-1][y].setValue(blocks[x][y].getValue());
-                            blocks[x][y].setValue(0);
-                            moved = true;
-                        } else if (blocks[x-1][y].getValue()==blocks[x][y].getValue()) {
-                            blocks[x-1][y].setValue(blocks[x][y].getValue()*2);
-                            blocks[x][y].setValue(0);
+    private synchronized void moveRight(App app, boolean initial) {
+        moving = true;
+        Runnable runnable = () -> {
+            boolean moved = false;
+            boolean merged = false;
+            for (int x = blocks.length - 1; x >= 0; x--) {
+                if (x!=blocks.length-1) {
+                    for (int y = 0; y < blocks[x].length; y++) {
+                        if (blocks[x][y].getValue()!=0) {
+                            if (blocks[x+1][y].getValue()==0) {
+                                blocks[x+1][y].setValue(blocks[x][y].getValue());
+                                blocks[x][y].setValue(0);
+                                moved = true;
+                            } else if (blocks[x+1][y].getValue()==blocks[x][y].getValue() && !blocks[x+1][y].isBlocked()) {
+                                blocks[x+1][y].setValue(blocks[x][y].getValue()*2);
+                                blocks[x][y].setValue(0);
+                                score+=blocks[x+1][y].getValue();
+                                blocks[x+1][y].setBlocked(true);
+                                merged = true;
+                            }
                         }
                     }
                 }
             }
-        }
-        if (moved) moveLeft(app, false);
-        else if (!initial) spawn(app);
+            if (moved) moveRight(app, false);
+            else if (!initial || merged) {
+                spawn(app);
+                endMove();
+            }
+        };
+        if (!initial) {
+            try {
+                Thread.sleep(animationDelay);
+                runnable.run();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        } else new Thread(runnable).start();
     }
 
-    private void moveDown(App app, boolean initial) {
-        boolean moved = false;
-        for (Block[] block : blocks) {
-            for (int y = block.length - 1; y >= 0; y--) {
-                if (y != block.length - 1) {
-                    if (block[y].getValue() != 0) {
-                        if (block[y + 1].getValue() == 0) {
-                            block[y + 1].setValue(block[y].getValue());
-                            block[y].setValue(0);
-                            moved = true;
-                        } else if (block[y + 1].getValue() == block[y].getValue()) {
-                            block[y + 1].setValue(block[y].getValue() * 2);
-                            block[y].setValue(0);
+    private synchronized void moveLeft(App app, boolean initial) {
+        moving = true;
+        Runnable runnable = () -> {
+            boolean moved = false;
+            boolean merged = false;
+            for (int x = 0; x < blocks.length; x++) {
+                if (x!=0) {
+                    for (int y = 0; y < blocks[x].length; y++) {
+                        if(blocks[x][y].getValue()!=0) {
+                            if (blocks[x-1][y].getValue()==0) {
+                                blocks[x-1][y].setValue(blocks[x][y].getValue());
+                                blocks[x][y].setValue(0);
+                                moved = true;
+                            } else if (blocks[x-1][y].getValue()==blocks[x][y].getValue() && !blocks[x-1][y].isBlocked()) {
+                                blocks[x-1][y].setValue(blocks[x][y].getValue()*2);
+                                blocks[x][y].setValue(0);
+                                score+=blocks[x-1][y].getValue();
+                                blocks[x-1][y].setBlocked(true);
+                                merged = true;
+                            }
                         }
                     }
                 }
             }
-        }
-        if (moved) moveDown(app, false);
-        else if (!initial) spawn(app);
+            if (moved) moveLeft(app, false);
+            else if (!initial || merged) {
+                spawn(app);
+                endMove();
+            }
+        };
+        if (!initial) {
+            try {
+                Thread.sleep(animationDelay);
+                runnable.run();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        } else new Thread(runnable).start();
     }
 
-    private void moveUp(App app, boolean initial) {
-        boolean moved = false;
-        for (Block[] block : blocks) {
-            for (int y = 0; y < block.length; y++) {
-                if (y!=0) {
-                    if (block[y].getValue()!=0) {
-                        if (block[y-1].getValue()==0) {
-                            block[y-1].setValue(block[y].getValue());
-                            block[y].setValue(0);
-                            moved = true;
-                        } else if (block[y-1].getValue()==block[y].getValue()) {
-                            block[y-1].setValue(block[y].getValue()*2);
-                            block[y].setValue(0);
+    private synchronized void moveDown(App app, boolean initial) {
+        moving = true;
+        Runnable runnable = () -> {
+            boolean moved = false;
+            boolean merged = false;
+            for (Block[] block : blocks) {
+                for (int y = block.length - 1; y >= 0; y--) {
+                    if (y != block.length - 1) {
+                        if (block[y].getValue() != 0) {
+                            if (block[y + 1].getValue() == 0) {
+                                block[y + 1].setValue(block[y].getValue());
+                                block[y].setValue(0);
+                                moved = true;
+                            } else if (block[y + 1].getValue() == block[y].getValue() && !block[y+1].isBlocked()) {
+                                block[y + 1].setValue(block[y].getValue() * 2);
+                                block[y].setValue(0);
+                                score+=block[y+1].getValue();
+                                block[y+1].setBlocked(true);
+                                merged = true;
+                            }
                         }
                     }
                 }
             }
-        }
-        if (moved) moveUp(app, false);
-        else if (!initial) spawn(app);
+            if (moved) moveDown(app, false);
+            else if (!initial || merged) {
+                spawn(app);
+                endMove();
+            }
+        };
+        if (!initial) {
+            try {
+                Thread.sleep(animationDelay);
+                runnable.run();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        } else new Thread(runnable).start();
+    }
+
+    private synchronized void moveUp(App app, boolean initial) {
+        moving = true;
+        Runnable runnable = () -> {
+            boolean moved = false;
+            boolean merged = false;
+            for (Block[] block : blocks) {
+                for (int y = 0; y < block.length; y++) {
+                    if (y!=0) {
+                        if (block[y].getValue()!=0) {
+                            if (block[y-1].getValue()==0) {
+                                block[y-1].setValue(block[y].getValue());
+                                block[y].setValue(0);
+                                moved = true;
+                            } else if (block[y-1].getValue()==block[y].getValue() && !block[y-1].isBlocked()) {
+                                block[y-1].setValue(block[y].getValue()*2);
+                                block[y].setValue(0);
+                                score+=block[y-1].getValue();
+                                block[y-1].setBlocked(true);
+                                merged = true;
+                            }
+                        }
+                    }
+                }
+            }
+            if (moved) moveUp(app, false);
+            else if (!initial || merged) {
+                spawn(app);
+                endMove();
+            }
+        };
+        if (!initial) {
+            try {
+                Thread.sleep(animationDelay);
+                runnable.run();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        } else new Thread(runnable).start();
     }
 }
